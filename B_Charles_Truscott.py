73ed92aa59f00584743886cc7bf6ccad140ad98e
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2023-02-02 23:36:00 +1100

    Add files via upload

--- B_Charles_Truscott.py
+++ B_Charles_Truscott.py
@@ -1,4 +1,4 @@
-def Charles(L):
+def Charles(L, steps):
 	""" B = [1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1] ...
 	
 [1, 0]
@@ -34,20 +34,223 @@ def Charles(L):
 [0, 0]
 [1, 1]
 
+Input Size: 64
+One plus the logarithm to base 2 of the input size, e.g. tree size or divide and conquer size: 7.0
+[1]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[0]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+[1]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[0]
+Recursive calls, steps, tree size: 7
+[1]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+Recursive calls, steps, tree size: 4
+[1]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[1]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+[1]
+Recursive calls, steps, tree size: 7
+[1]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[1]
+Recursive calls, steps, tree size: 7
+[1]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+Recursive calls, steps, tree size: 4
+Recursive calls, steps, tree size: 3
+[1]
+Recursive calls, steps, tree size: 7
+[1]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[1]
+Recursive calls, steps, tree size: 7
+[1]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+[1]
+Recursive calls, steps, tree size: 7
+[1]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[1]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+Recursive calls, steps, tree size: 4
+[1]
+Recursive calls, steps, tree size: 7
+[1]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[0]
+Recursive calls, steps, tree size: 7
+[1]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+[1]
+Recursive calls, steps, tree size: 7
+[1]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[0]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+Recursive calls, steps, tree size: 4
+Recursive calls, steps, tree size: 3
+Recursive calls, steps, tree size: 2
+[1]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[1]
+Recursive calls, steps, tree size: 7
+[1]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+[1]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[1]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+Recursive calls, steps, tree size: 4
+[1]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[0]
+Recursive calls, steps, tree size: 7
+[1]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+[1]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[0]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+Recursive calls, steps, tree size: 4
+Recursive calls, steps, tree size: 3
+[0]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[0]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+[0]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[0]
+Recursive calls, steps, tree size: 7
+[1]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+Recursive calls, steps, tree size: 4
+[0]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[1]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+[0]
+Recursive calls, steps, tree size: 7
+[0]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+[1]
+Recursive calls, steps, tree size: 7
+[1]
+Recursive calls, steps, tree size: 7
+Recursive calls, steps, tree size: 6
+Recursive calls, steps, tree size: 5
+Recursive calls, steps, tree size: 4
+Recursive calls, steps, tree size: 3
+Recursive calls, steps, tree size: 2
+Recursive calls, steps, tree size: 1
+
+[Program finished]
+
 """
-	
-	l = L[0:len(L)//2]
-	r = L[len(L)//2:len(L)]
-	if len(L) == 2:
-		print(L)
-	else:
-		Charles(l)
-		Charles(r)
+	try:
+		l = L[0:len(L)//2]
+		r = L[len(L)//2:len(L)]
+		steps += 1
+		if len(L) == 1:
+			print(L)
+		else:
+			Charles(l, steps)
+			Charles(r, steps)
+	except ValueError as e:
+		print("Error: {}".format(e))
+	finally:
+		print("Recursive calls, steps, tree size: {}".format(steps))
 	
 	
 
 B = [1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1]
-print(len(B))
-print(4 * 16)
+print("Input Size: {}".format(len(B)))
+#print(4 * 16)
+import numpy as np
+print("One plus the logarithm to base 2 of the input size, e.g. tree size or divide and conquer size: {}".format(1 + np.log2(64)))
 
-if __name__ == "__main__": Charles(B)
+if __name__ == "__main__": Charles(B, 0)
